8b23c77038f8f09368f9cc24ad876529a10bdbbe
SOUP <soupbowlgamestudio@gmail.com>
2026-06-26 22:08:52 +1000

    added name and image field to card

--- souper-serious-game-jam/Cards_And_Actions/Card.gd
+++ souper-serious-game-jam/Cards_And_Actions/Card.gd
@@ -22,17 +22,14 @@ enum Casting_Type
 @export var Cost : int 
 @export var Base_Action : Action
 
+@export var Name : String = ""
 @export_multiline var Description : String = ""
+@export var Art : Image
 
 @export_flags("Self", "Friendlies", "Hostiles") var Applicability_Flags : int = 0
 
 func Get_Action_Data() -> ActionData:
 	return Base_Action.Generate_Action_Data()
-
-func Take_Cards_Action(_world_state : World_State) -> void:
-	pass
-	
-	#Base_Action.Take_Action(_world_state, self)
 
 func Has_Attribute_List(_attributes : Array[Atrributes]) -> bool:
 	for _att : Atrributes in _attributes:
