80e64d38e486ee6d47a8b2f52a6c389844f13fdf
Erica-99 <turbocake17@gmail.com>
2026-06-26 20:01:40 +1000

    Simplified trigger conditions

--- souper-serious-game-jam/Scripts/Battle_System/Logic/Status_Effects/Status_Effect_Effects/ClassTraitorEffect.gd
+++ souper-serious-game-jam/Scripts/Battle_System/Logic/Status_Effects/Status_Effect_Effects/ClassTraitorEffect.gd
@@ -3,6 +3,7 @@ class_name ClassTraitorEffect
 
 func apply_effect_to_action(action_data: ActionData, effect_stacks: int) -> ActionData:
 	if (action_data.caster.Team == Base_Actor.ActorTeam.Player 
-	and action_data.target == action_data.caster):
+	and action_data.target == action_data.caster 
+	and action_data.action_type == action_data.DamageType.Damage):
 		action_data.target.Energy += int(action_data.action_value * 0.5 * effect_stacks)
 	return action_data
--- souper-serious-game-jam/Scripts/Battle_System/Logic/Status_Effects/Status_Effect_Effects/RhetoricEffect.gd
+++ souper-serious-game-jam/Scripts/Battle_System/Logic/Status_Effects/Status_Effect_Effects/RhetoricEffect.gd
@@ -4,5 +4,5 @@ class_name RhetoricEffect
 @export var ConvictionStatusEffect: StatusEffect
 
 func apply_effect_to_action(action_data: ActionData, effect_stacks: int) -> ActionData:
-	# Need code here to apply -1 Conviction to a random enemy
+	# Need code here to apply -1 Conviction to a random enemy if dealing damage
 	return action_data
