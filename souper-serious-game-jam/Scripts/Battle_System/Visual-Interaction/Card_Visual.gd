9e886d08211e216e7fa065d11f9bf3d378f3bd15
SOUP <soupbowlgamestudio@gmail.com>
2026-06-26 22:18:12 +1000

    added title functionality and image field to cards

--- souper-serious-game-jam/Scripts/Battle_System/Visual-Interaction/Card_Visual.gd
+++ souper-serious-game-jam/Scripts/Battle_System/Visual-Interaction/Card_Visual.gd
@@ -7,6 +7,9 @@ signal Card_Let_Go(_card : Card_Visual)
 @onready var polygon_2d: Polygon2D = $Polygon2D
 @onready var desc_lable: Label = $Control/MarginContainer/Desc_Lable
 @onready var cost_lable: Label = $Control/Cost_Lable
+@onready var name_lable: Label = $Control/Name_lable
+
+@onready var mouse_detector: Mouse_Detector = $MouseDetector
 
 func Set_Visual_Based_Off_Card(_card : Card) -> void:
 	if _card.Has_Attribute(Card.Atrributes.Red):
@@ -24,9 +27,18 @@ func Set_Visual_Based_Off_Card(_card : Card) -> void:
 	cost_lable.set_text(
 		str(_card.Cost)
 	)
+	name_lable.set_text(
+		_card.Name
+	)
 
-func _on_control_button_down() -> void:
-	Card_Being_Held.emit(self)
-
-func _on_control_button_up() -> void:
-	Card_Let_Go.emit(self)
+var holding : bool = false
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Left_Click"):
+		if mouse_detector.Mouse_Within:
+			holding = true
+			Card_Being_Held.emit(self)
+	
+	if Input.is_action_just_released("Left_Click"):
+		if holding:
+			holding = false
+			Card_Let_Go.emit(self)
